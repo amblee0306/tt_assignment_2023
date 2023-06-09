8dbae1cf9af3df0d3d56bfcb2a3d23941ee889f9
Amber <amblee0306@gmail.com>
2023-06-10 02:08:46 +0800

    Store messages into a list and retrive them.

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"math"
 	"math/rand"
 	"os"
 
@@ -20,13 +23,16 @@ func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.Se
 	log.Printf("req %v %v %v %v", req.GetMessage().GetChat(), req.GetMessage().GetSender(),
 		req.GetMessage().GetSendTime(), req.GetMessage().GetText())
 
-	redisErr := s.redisClient.Set(req.GetMessage().GetChat(), req.GetMessage().GetText(), 0).Err()
+	// redisErr := s.redisClient.Set(req.GetMessage().GetChat(), req.GetMessage().GetText(), 0).Err()
+	b, _ := json.Marshal(req.GetMessage())
+	redisErr := s.redisClient.LPush(req.GetMessage().GetChat(), string(b)).Err()
 	if redisErr != nil {
 		log.Println("RPC Send, set redis fail, ", redisErr)
+		resp.Code = 500
 		return nil, redisErr
 	}
 
-	resp.Code, resp.Msg = areYouLuckySend(req)
+	resp.Code = 0
 	return resp, nil
 }
 
@@ -34,14 +40,38 @@ func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.Pu
 	resp := rpc.NewPullResponse()
 
 	log.Println("Chat", req.GetChat())
-	textVal, redisErr := s.redisClient.Get(req.GetChat()).Result()
+
+	// textVal, redisErr := s.redisClient.Get(req.GetChat()).Result()
+	dbvalJsons, redisErr := s.redisClient.LRange(req.GetChat(), req.GetCursor(), int64(req.GetLimit())).Result()
+	var dbvals []rpc.Message
+	for _, dbValJson := range dbvalJsons {
+		dbval := rpc.Message{}
+		err := json.Unmarshal([]byte(dbValJson), &dbval)
+		if err != nil {
+			panic(fmt.Sprintf("amber boo %v", err))
+		}
+		log.Println("dbval", dbval)
+
+		dbvals = append(dbvals, dbval)
+	}
+	log.Println("dbvals", dbvals)
+
 	if redisErr != nil {
 		log.Println("RPC Pull, get redis fail, ", redisErr)
+		resp.Code = 500
 		return nil, redisErr
 	}
-	log.Println("Returned text from redis, ", textVal)
+	log.Println("Returned text from redis, ", dbvals)
 
-	resp.Code, resp.Msg = areYouLuckyPull()
+	resp.Code = 0
+
+	resp.Messages = make([]*rpc.Message, int(math.Min(float64(req.GetLimit())+1, float64(len(dbvals)))))
+	// For Loop to insert data
+	for i := 0; i < len(dbvals) && i < int(req.GetLimit()+1); i++ {
+		// resp.Messages[i] = &rpc.Message{}
+		resp.Messages[i] = &dbvals[i]
+	}
+
 	return resp, nil
 }
 
